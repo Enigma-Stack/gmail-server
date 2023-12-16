af5ee4368a5b5adce9af509c443012b2a1bb0c31
Aryamanraj <20cs3011@rgipt.ac.in>
2023-12-16 19:32:02 +0530

    config controller

---
+++ src/config.js
@@ -0,0 +1,10 @@
+require('dotenv').config();
+
+module.exports = {
+  port: process.env.PORT || 5000,
+  clientId: process.env.CLIENT_ID,
+  apiKey: process.env.API_KEY,
+  redirectUri: process.env.REDIRECT_URI,
+//   frontEndRedirectUrl: process.env.FRONTEND_REDIRECT_URL
+  // Add other configuration variables as needed
+};
